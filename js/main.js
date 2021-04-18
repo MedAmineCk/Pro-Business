4dd6db54f4f856d05bb7793a6deac3c8bcbd8e04
MedAmineCk <medamineck4u@gmail.com>
2021-04-18 00:36:11 +0000

    video player

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(".player-btn_container").on("click", function () {
   $(".video_container").removeClass("hidden");
   $(".videoWrapper").append(
-    '<iframe width="560" height="349" src="https://www.youtube.com/embed/6Gu2QMTAkEU?autoplay=1&mute=1" frameborder="0"allowfullscreen></iframe>'
+    '<iframe width="560" height="349" src="https://www.youtube.com/embed/6Gu2QMTAkEU?autoplay=1" frameborder="0"allowfullscreen></iframe>'
   );
 });
 
